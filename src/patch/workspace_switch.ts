0ac59a787730521ea1cd35555d8aaa63ba2eb572
p-sage <171166538+p-sage@users.noreply.github.com>
2025-09-08 09:30:42 +0200

    Fixed typo

--- src/patch/workspace_switch.ts
+++ src/patch/workspace_switch.ts
@@ -50,7 +50,7 @@ export function addShadowsInWorkspaceSwitch(
             for (const {windowActor: actor, clone} of windowRecords) {
                 // Skip windows that don't have a wm_class.
                 const win = actor.metaWindow;
-                 if (!win || !win.wm_class) {
+                if (!win || !win.wm_class) {
                     continue;
                 }
 
